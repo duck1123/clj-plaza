2ac01bd0c5743d35379c8789f969db4533334e8d
Daniel E. Renfer <duck@kronkltd.net>
2012-09-07 23:45:00 -0400

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject net.kronkltd/plaza "0.1.0-SNAPSHOT"
   :description "Plaza framework for semantic distributed applications"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.0"]
-                 [org.clojure/tools.logging "0.1.2"]
-                 [org.apache.jena/jena-core "2.7.0-incubating"]
-                 [org.apache.jena/jena-arq "2.9.0-incubating"]
-                 [net.rootdev/java-rdfa "0.4.2-RC2"]
-                 [com.franz/openrdf-sesame-onejar "2.2"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/data.json "0.1.3"]
+                 [org.clojure/tools.logging "0.2.4"]
+                 [org.apache.jena/jena-core "2.7.2"]
+                 [org.apache.jena/jena-arq "2.9.2"]
+                 [net.rootdev/java-rdfa "0.4.2"]
+                 [com.franz/openrdf-sesame-onejar "2.3.1"]]
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "apache-repo-release" "https://repository.apache.org/content/repositories/releases/"}
   :dev-dependencies [[log4j "1.2.14"]]
