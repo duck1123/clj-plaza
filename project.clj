c25f95ea660457293971d16cb264d51391f52351
Daniel E. Renfer <duck@kronkltd.net>
2013-05-31 07:56:37 -0400

    add url to project

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject net.kronkltd/plaza "0.2.0-SNAPSHOT"
   :description "Plaza framework for semantic distributed applications"
+  :url "http://github.com/duck1123/clj-plaza"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.3"]
                  [org.clojure/tools.logging "0.2.4"]
