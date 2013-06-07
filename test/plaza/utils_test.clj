f29321585527eb000679277a2f1f688219b7c8f1
Daniel E. Renfer <duck@kronkltd.net>
2013-06-07 10:56:10 -0400

    utils test

--- test/plaza/utils_test.clj
+++ test/plaza/utils_test.clj
@@ -1,26 +1,27 @@
 (ns plaza.utils-test
-  (:use plaza.utils
-        clojure.test))
+  (:use [plaza.utils :only [check-default-values cmd-param->keyword
+                            flatten-1 fold-list]]
+        [midje.sweet :only [=> fact]]))
 
-(deftest test-fold-list
-  (is (= (fold-list [1 2 3 4])
-         [[1 2] [3 4]])))
+(fact "check-default-values"
+  (let [optsp (check-default-values {:password "foo"} {:password "bar" :username "guest"})]
+    (:password optsp) => "foo"
+    (:username optsp) => "guest"))
 
-(deftest test-flatten-1
-  (is (= (set (flatten-1 [:a [[:s :p :o] [:s :p :o]] :c [[:s :p :o]] :d]))
-         (set [:a [:s :p :o] :c :d]))))
+(fact "cmd-param->keywords"
+  (cmd-param->keyword "hola") => "hola"
+  (cmd-param->keyword :hola) => :hola
+  (cmd-param->keyword "-hola") => :hola)
 
-(deftest test-flatten-1-preserves-meta
+(fact "flatten-1"
+  (->> [:a [[:s :p :o] [:s :p :o]] :c [[:s :p :o]] :d]
+       flatten-1
+       (into #{})) => #{:a [:s :p :o] :c :d})
+
+(fact "flatten-1-preserves-meta"
   (let [to-flatten (with-meta [:a [[:s :p :o] [:s :p :o]] :c [[:s :p :o]] :d] {:flatten true})
         flattened (flatten-1 to-flatten)]
-    (is (= (meta to-flatten) (meta flattened)))))
+    (meta to-flatten) => (meta flattened)))
 
-(deftest test-cmd-param->keywords
-  (is (= "hola" (cmd-param->keyword "hola")))
-  (is (= :hola (cmd-param->keyword :hola)))
-  (is (= :hola (cmd-param->keyword "-hola"))))
-
-(deftest test-check-default-values
-  (let [optsp (check-default-values {:password "foo"} {:password "bar" :username "guest"})]
-    (is (= "foo" (:password optsp)))
-    (is (= "guest" (:username optsp)))))
+(fact "fold-list"
+  (fold-list [1 2 3 4]) => [[1 2] [3 4]])
