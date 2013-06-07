835e52b3deee723b4a84b8438fccd29532028232
Daniel E. Renfer <duck@kronkltd.net>
2013-06-07 14:02:56 -0400

    schemas test

--- test/plaza/rdf/schemas_test.clj
+++ test/plaza/rdf/schemas_test.clj
@@ -17,63 +17,60 @@
                     :price  {:uri ["http://test.com/" :price] :range :float}
                     :number {:uri :number                     :range :int}))
 
-(deftest test-extend-schemas
+(fact "extend-schemas"
   (let [extended (extend-rdfs-schemas "http://test.com/Foo" [*test-model*])]
-    (is (= "http://test.com/Foo" (str (type-uri extended))))
-    (is (some (partial = "http://test.com/Foo")
-              (map #(str %1) (super-type-uris extended)) ))
-    (is (some (partial =  "http://something/Good")
-              (map #(str %1) (super-type-uris extended))))
-    (is (= "http://test.com/name" (str (property-uri extended :name))))))
+    (str (type-uri extended)) => "http://test.com/Foo"
+    (some #{"http://test.com/Foo"} (map str (super-type-uris extended))) => truthy
+    (some #{"http://something/Good"} (map str (super-type-uris extended))) => truthy
+    (str (property-uri extended :name)) => "http://test.com/name"))
 
-(deftest test-props
-  (is (= "http://something/Good" (str (type-uri *test-model*)))))
+(fact "props"
+  (str (type-uri *test-model*)) => "http://something/Good")
 
-(deftest test-add-remove-prop
+(fact "add-remove-prop"
   (do (let [modelp (-> *test-model*
                        (add-property :wadus "http://test.com/wadus" :float)
                        (add-property :foo "http://test.com/foo" "http://test.com/ranges/foo"))
             modelpp (-> modelp
                         (remove-property-by-uri "http://test.com/foo")
                         (remove-property-by-alias :wadus))]
-        (= :foo (property-alias modelp "http://test.com/foo"))
-        (= :wadus (property-alias modelp "http://test.com/wadus"))
-        (is (nil? (property-alias modelpp :wadus)))
-        (is (nil? (property-alias modelpp :foo))))))
+        (property-alias modelp "http://test.com/foo") => :foo
+        (property-alias modelp "http://test.com/wadus") => :wadus
+        (property-alias modelpp :wadus) => nil?
+        (property-alias modelpp :foo) => nil?)))
 
-(deftest test->map
+(fact "to-map"
   (let [m (to-map *test-model* [[:test ["http://test.com/" :name] "name"]
                                 [:test ["http://test.com/" :price] (d 120)]
                                 [:test :number (d 10)]])]
-    (is (= m {:name (rdf-resource "name") :price (d 120) :number (d 10)}))))
+    m => {:name (rdf-resource "name") :price (d 120) :number (d 10)}))
 
-(deftest test->pattern
+(fact "to-pattern"
   (let [p (to-pattern *test-model* [:name :price])]
-    (is (= 4 (count p)))
-    (is (= 2 (count (filter #(:optional (meta %1)) p))))))
+    (count p) => 4
+    (count (filter #(:optional (meta %1)) p)) => 2))
 
-(deftest test->pattern-2
+(fact "to-pattern-2"
   (let [p (to-pattern *test-model* "http://test.com/Test" [:name :price])]
-    (is (= 4 (count p)))
-    (is (= 2 (count (filter #(:optional (meta %1)) p))))
-    (doseq [[s _p _o] p] (is (= "http://test.com/Test" (resource-id s))))))
+    (count p) => 4
+    (count (filter #(:optional (meta %1)) p)) => 2
+    (doseq [[s _p _o] p]
+      (resource-id s) => "http://test.com/Test")))
 
-(deftest test-property-uri
-  (is (= "http://test.com/name" (str (property-uri *test-model* :name)))))
+(fact "property-uri"
+  (str (property-uri *test-model* :name)) => "http://test.com/name")
 
-(deftest test-property-alias
-  (is (= :name (property-alias *test-model* "http://test.com/name"))))
+(fact "property-alias"
+  (property-alias *test-model* "http://test.com/name") => :name)
 
-(deftest test-property-parse-value
-  (is (= 2 (parse-prop-value *test-model* :number "2"))))
+(fact "property-parse-value"
+  (parse-prop-value *test-model* :number "2") => 2)
 
-(deftest test-schema->triples
+(fact "schema->triples"
   (let [ts (to-rdf-triples foaf:Agent-schema)]
-    (is (= 37 (count ts)))))
+    (count ts) => 37))
 
 (fact "parse-from-rdf"
-  (init-sesame-framework)
-
   (let [ts (to-rdf-triples foaf:Agent-schema)
         *m* (build-model)
         _tmp (with-model *m* (model-add-triples ts))
