f8f2bb6778a160a362f9a7602d00102e7ab37674
Daniel E. Renfer <duck@kronkltd.net>
2014-03-29 00:19:05 -0400

    updates

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "Plaza framework for semantic distributed applications"
   :url "http://github.com/duck1123/clj-plaza"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.2"]
+                 [org.clojure/data.json "0.2.4"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.apache.jena/jena-core "2.10.1"]
-                 [org.apache.jena/jena-arq "2.10.1"]
+                 [org.apache.jena/jena-core "2.11.1"]
+                 [org.apache.jena/jena-arq "2.11.1"]
                  [net.rootdev/java-rdfa "0.4.2"]
                  [com.franz/openrdf-sesame-onejar "2.3.1"]]
   :min-lein-version "2.0.0"
@@ -14,7 +14,7 @@
   :profiles {:dev
              {:dependencies
               [[log4j "1.2.17"]
-               [midje "1.6-alpha1"]]}}
+               [midje "1.6.3"]]}}
   :plugins [[lein-midje "3.0.1"]
             [codox "0.6.1"]]
   :autodoc {:name "clj-plaza",
